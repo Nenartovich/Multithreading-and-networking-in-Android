2d26484436065f816854cc870ff782816480c746
Evgeny Nenartovich <nenartovski@yandex.com>
2021-06-27 00:39:54 +0300

    Added recipe detail activity

--- Week2/Examples/Example02_ListView/app/src/main/java/com/nenartovich/example02_listview/MainActivity.kt
+++ Week2/Examples/Example02_ListView/app/src/main/java/com/nenartovich/example02_listview/MainActivity.kt
@@ -2,6 +2,8 @@ package com.nenartovich.example02_listview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
 
@@ -18,5 +20,12 @@ class MainActivity : AppCompatActivity() {
 
         val adapter = RecipeAdapter(this, recipeList)
         listView.adapter = adapter
+
+        val context = this
+        listView.setOnItemClickListener { p0, p1, position, p3 ->
+            val selectedRecipe = recipeList[position]
+            val detailIntent = RecipeDetailActivity.newIntent(context, selectedRecipe)
+            startActivity(detailIntent)
+        }
     }
 }
