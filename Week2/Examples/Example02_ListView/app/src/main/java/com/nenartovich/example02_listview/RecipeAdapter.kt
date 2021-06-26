74e27f9486f053757fee15ab1e3b1b24144d5e14
Evgeny Nenartovich <nenartovski@yandex.com>
2021-06-27 00:04:09 +0300

    Added styling

--- Week2/Examples/Example02_ListView/app/src/main/java/com/nenartovich/example02_listview/RecipeAdapter.kt
+++ Week2/Examples/Example02_ListView/app/src/main/java/com/nenartovich/example02_listview/RecipeAdapter.kt
@@ -7,12 +7,26 @@ import android.view.ViewGroup
 import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import com.squareup.picasso.Picasso
 
 class RecipeAdapter(private val context: Context,
                     private val dataSource: ArrayList<Recipe>) : BaseAdapter() {
 
     private val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+
+    companion object {
+        private val LABEL_COLORS = hashMapOf(
+            "Low-Carb" to R.color.colorLowCarb,
+            "Low-Fat" to R.color.colorLowFat,
+            "Low-Sodium" to R.color.colorLowSodium,
+            "Medium-Carb" to R.color.colorMediumCarb,
+            "Vegetarian" to R.color.colorVegetarian,
+            "Balanced" to R.color.colorBalanced
+        )
+    }
+
 
     override fun getCount(): Int = dataSource.size
 
@@ -35,6 +49,18 @@ class RecipeAdapter(private val context: Context,
         detailTextView.text = recipe.label
 
         Picasso.with(context).load(recipe.imageUrl).placeholder(R.mipmap.ic_launcher).into(thumbnailImageView)
+
+        val titleTypeFace = ResourcesCompat.getFont(context, R.font.josefinsans_bold)
+        titleTextView.typeface = titleTypeFace
+
+        val subtitleTypeFace = ResourcesCompat.getFont(context, R.font.josefinsans_semibolditalic)
+        subtitleTextView.typeface = subtitleTypeFace
+
+        val detailTypeFace = ResourcesCompat.getFont(context, R.font.quicksand_bold)
+        detailTextView.typeface = detailTypeFace
+
+        detailTextView.setTextColor(
+            ContextCompat.getColor(context, LABEL_COLORS[recipe.label] ?: R.color.colorPrimary))
 
         return rowView
     }
