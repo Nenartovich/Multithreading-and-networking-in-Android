05e93fe9bfecbeef994bb686c41905e9ec127207
Evgeny Nenartovich <nenartovski@yandex.com>
2021-07-01 16:15:20 +0300

    Save information between screen rotation

--- Week2/Examples/Example04_GridView/app/src/main/java/com/nenartovich/example04_gridview/MainActivity.kt
+++ Week2/Examples/Example04_GridView/app/src/main/java/com/nenartovich/example04_gridview/MainActivity.kt
@@ -2,11 +2,38 @@ package com.nenartovich.example04_gridview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.PersistableBundle
 import android.view.View
 import android.widget.AdapterView
 import android.widget.GridView
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val FAVOURITE_BOOKS_NAMES_KEY = "com.nenartovich.FAVOURITE_BOOKS_NAMES_KEY"
+        private val bookArray  = arrayOf(
+        Book(R.string.abc_an_amazing_alphabet_book, R.string.dr_seuss, R.drawable.abc,
+        false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/abc.jpg"),
+        Book(R.string.are_you_my_mother, R.string.dr_seuss, R.drawable.areyoumymother,
+        false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/areyoumymother.jpg"),
+        Book(R.string.where_is_babys_belly_button, R.string.karen_katz, R.drawable.whereisbabysbellybutton,
+        false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/whereisbabysbellybutton.jpg"),
+        Book(R.string.on_the_night_you_were_born, R.string.nancy_tillman, R.drawable.onthenightyouwereborn,
+        false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/onthenightyouwereborn.jpg"),
+        Book(R.string.hand_hand_fingers_thumb, R.string.dr_seuss, R.drawable.handhandfingersthumb,
+        false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/handhandfingersthumb.jpg"),
+        Book(R.string.the_very_hungry_caterpillar, R.string.eric_carle, R.drawable.theveryhungrycaterpillar,
+        false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/theveryhungrycaterpillar.jpg"),
+        Book(R.string.the_going_to_bed_book, R.string.sandra_boynton, R.drawable.thegoingtobedbook,
+        false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/thegoingtobedbook.jpg"),
+        Book(R.string.oh_baby_go_baby, R.string.dr_seuss, R.drawable.ohbabygobaby,
+        false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/ohbabygobaby.jpg"),
+        Book(R.string.the_tooth_book, R.string.dr_seuss, R.drawable.thetoothbook,
+        false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/thetoothbook.jpg"),
+        Book(R.string.one_fish_two_fish_red_fish_blue_fish, R.string.dr_seuss, R.drawable.onefish,
+        false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/onefish.jpg"))
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -14,7 +41,7 @@ class MainActivity : AppCompatActivity() {
 
 
         val gridView: GridView = findViewById(R.id.gridview)
-        val booksAdapter = BooksAdapter(this, getBooks())
+        val booksAdapter = BooksAdapter(this, bookArray)
         gridView.onItemClickListener = AdapterView.OnItemClickListener {
             p0, p1, p2, p3 ->
             val book = booksAdapter.getItem(p2) as Book
@@ -27,27 +54,32 @@ class MainActivity : AppCompatActivity() {
 
     }
 
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val favouriteBooksNames = arrayListOf<Int>()
+        for (book in bookArray) {
+            if (book.isFavourite) {
+                favouriteBooksNames.add(book.name)
+            }
+        }
+        outState.putIntegerArrayList(FAVOURITE_BOOKS_NAMES_KEY, favouriteBooksNames)
+    }
 
-    private fun getBooks() : Array<Book>  = arrayOf(
-        Book(R.string.abc_an_amazing_alphabet_book, R.string.dr_seuss, R.drawable.abc,
-            false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/abc.jpg"),
-        Book(R.string.are_you_my_mother, R.string.dr_seuss, R.drawable.areyoumymother,
-             false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/areyoumymother.jpg"),
-        Book(R.string.where_is_babys_belly_button, R.string.karen_katz, R.drawable.whereisbabysbellybutton,
-             false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/whereisbabysbellybutton.jpg"),
-        Book(R.string.on_the_night_you_were_born, R.string.nancy_tillman, R.drawable.onthenightyouwereborn,
-            false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/onthenightyouwereborn.jpg"),
-        Book(R.string.hand_hand_fingers_thumb, R.string.dr_seuss, R.drawable.handhandfingersthumb,
-            false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/handhandfingersthumb.jpg"),
-        Book(R.string.the_very_hungry_caterpillar, R.string.eric_carle, R.drawable.theveryhungrycaterpillar,
-            false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/theveryhungrycaterpillar.jpg"),
-        Book(R.string.the_going_to_bed_book, R.string.sandra_boynton, R.drawable.thegoingtobedbook,
-            false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/thegoingtobedbook.jpg"),
-        Book(R.string.oh_baby_go_baby, R.string.dr_seuss, R.drawable.ohbabygobaby,
-            false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/ohbabygobaby.jpg"),
-        Book(R.string.the_tooth_book, R.string.dr_seuss, R.drawable.thetoothbook,
-            false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/thetoothbook.jpg"),
-        Book(R.string.one_fish_two_fish_red_fish_blue_fish, R.string.dr_seuss, R.drawable.onefish,
-            false, "http://www.raywenderlich.com/wp-content/uploads/2016/03/onefish.jpg"))
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        val favouriteBooksNames = savedInstanceState.getIntegerArrayList(FAVOURITE_BOOKS_NAMES_KEY)
+
+        for (favouriteBookName in favouriteBooksNames!!) {
+            for (book in bookArray) {
+                if (book.name == favouriteBookName) {
+                    book.isFavourite = true
+                    break
+                }
+            }
+        }
+    }
+
+
+
 
 }
