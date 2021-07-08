ebb72c3ef26f12da12e0a139f2371575a609b203
Evgeny Nenartovich <nenartovski@yandex.com>
2021-07-08 15:27:51 +0300

    Added custom toolbar

--- Week2/Task1_1/app/src/main/java/com/nenartovich/task1_1/MainActivity.kt
+++ Week2/Task1_1/app/src/main/java/com/nenartovich/task1_1/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.nenartovich.task1_1
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.Menu
+import android.view.MenuItem
+import androidx.appcompat.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,5 +16,28 @@ class MainActivity : AppCompatActivity() {
             .beginTransaction()
             .replace(R.id.fl_main_container, RecyclerFragment.newInstance(), null)
             .commit()
+
+        val toolbar: Toolbar = findViewById(R.id.tb_toolbar)
+        toolbar.setTitleTextColor(Color.WHITE)
+        setSupportActionBar(toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.btn_add_text -> {
+                Log.d("TAG", "onOptionsItemSelected: Text ")
+            }
+
+            R.id.btn_add_image -> {
+                Log.d("TAG", "onOptionsItemSelected: Image")
+            }
+        }
+        return true
     }
 }
